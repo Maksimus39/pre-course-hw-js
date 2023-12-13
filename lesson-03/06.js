42727fd0b7d0f56d16e92cf0354fd1ae723e57e2
Macsim Minakov <maksim.minakov.83@mail.ru>
2023-12-13 22:22:18 +0300

    06.js

---
+++ lesson-03/06.js
@@ -0,0 +1,39 @@
+const users3 = [
+    {
+        id: 1,
+        user: 'John',
+        surName: 'Doe',
+        address: '123 Main St',
+        isMarried:true
+    },
+    {
+        id: 2,
+        user: 'Tom',
+        surName: 'Johnson',
+        address: '789 Oak St',
+        isMarried:true
+    },
+    {
+        id: 3,
+        user: 'Bob',
+        surName: 'Jones',
+        address: '135 Pine St',
+        isMarried:true
+    },
+    {
+        id: 4,
+        user: 'David',
+        surName: 'Davis',
+        address: '864 Walnut St',
+        isMarried:true
+    },
+    {
+        id: 5,
+        user: 'Mark',
+        surName: 'Taylor',
+        address: '321 Hickory St',
+        isMarried:true
+    },
+];
+
+console.log(users3)
